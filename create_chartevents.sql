38be2c0f8138e4b9bd4ea15db602d2341f4310b7
Erin Hong <erin.hong17@gmail.com>
2016-12-08 17:57:52 -0500

    wrote script for final ace table

--- create_chartevents.sql
+++ create_chartevents.sql
@@ -37,9 +37,20 @@ SELECT subject_id FROM (
 WHERE numRows =3; 
 
 CREATE VIEW alt_chartevents AS
-SELECT a.* FROM alt_chartevents a 
+SELECT a.* FROM pre_chartevents a 
 INNER JOIN uniquePatients
 ON a.SUBJECT_ID = uniquePatients.SUBJECT_ID;
+
+################################################
+
+SELECT ace1.subject_id,
+	ace1.val heartrate,
+	ace2.val bloodpressure,
+	ace3.val resprate
+FROM alt_chartevents ace1
+JOIN alt_chartevents ace2 ON ace1.subject_id = ace2.subject_id AND ace2.label = 'bloodpressure'
+JOIN alt_chartevents ace3 ON ace1.subject_id = ace3.subject_id AND ace3.label = 'resprate'
+WHERE ace1.label = 'heartrate';
 
 
 
