1ab8656f4be15bfcd7b81475f9cd69afdf520bd2
Erin Hong <erin.hong17@gmail.com>
2016-12-10 00:21:31 -0500

    created gap inducer file

---
+++ gap_inducer.py
@@ -0,0 +1,14 @@
+def count(): 
+	numRows = 0 
+	with open("../ace.csv") as f:
+		reader = csv.reader(f,delimiter = ",")
+	    data = list(reader)
+	    row_count = len(data)
+	return numRows 
+
+def induceGaps(): 
+	fdafdsfa
+	write out to files 
+
+if __name__=='__main__':
+	gradient = [.1,.3,.5] 
